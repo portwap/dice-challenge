abcf85bf60ea188029b71c960d53a13d79294ab3
portwap <69810706+portwap@users.noreply.github.com>
2021-01-11 22:02:29 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ document.querySelector("img.img1").setAttribute("src", randomDice[randomNumber1]
 document.querySelector("img.img2").setAttribute("src", randomDice[randomNumber2]);
 
 if (randomNumber1 === randomNumber2) {
-  document.querySelector("h1").innerHTML = "Draw";
+  document.querySelector("h1").innerHTML = "❤️Draw❤️";
 } else if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "✔️Player 1 Wins!";
 } else if (randomNumber1 < randomNumber2) {
